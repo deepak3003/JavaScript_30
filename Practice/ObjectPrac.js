b8150f5e0a32651c1d8d9dd56127c900ee924969
deepak3003 <deepaksingh76152@gmail.com>
2025-03-31 17:43:56 +0530

    Using Object.entries()

--- Practice/ObjectPrac.js
+++ Practice/ObjectPrac.js
@@ -12,13 +12,47 @@ animal.height = 2.5 + 'feet';
 // console.log(animal);
 
 //Accessing Object Properties
-console.log(animal.color);
-console.log(animal["color"]);
+// console.log(animal.color);
+// console.log(animal["color"]);
 
 //JavaScript Object Methods
-console.log(person.fullName());
+// console.log(person.fullName());
 
 //JavaScript Objects are Mutable
+const x = person;
+x.age = 10;
+// console.log(person);
+
+// Deleting Properties
+// The delete keyword deletes a property from an object
+
+delete person.age;
+// console.log(person);
+
+// Nested Objects
+myObj = {
+    name:"Jhon",
+    age:30,
+    myCars:{
+        car1:"BMW",
+        car2: "Ford"
+    }
+}
+// console.log(myObj.myCars.car2);
+let text ="";
+for(let x in animal){
+    text += animal[x] +" ";
+}
+// document.getElementById("demo").innerHTML = text;
+
+// Using Object.entries()
+
+const fruits = {Bananas:300, Apple:500, Orange:200};
+let text1 ="";
+for(let[fruit, value] of Object.entries(fruits)){
+    text1 += fruit + ": "+value+"<br>";
+}
+// document.getElementById("demo").innerHTML = text1;
 
 
 
