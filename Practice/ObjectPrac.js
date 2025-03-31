c46c453ca0e4fba1047c1af8d679a6a1f1e6b24a
deepak3003 <deepaksingh76152@gmail.com>
2025-03-31 16:10:31 +0530

    Object Practice

---
+++ Practice/ObjectPrac.js
@@ -0,0 +1,7 @@
+
+const person = { firstName: "Deepak", lastName: "Singh", age: 22, eyeColor: "black" }
+console.log(person);
+const animal = new Object();
+animal.name = "Dog";
+animal.color ="Brown";
+animal.weight= ""
