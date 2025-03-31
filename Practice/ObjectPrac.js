376ca27ddbec712dd03e338825989ef9f02d10c6
deepak3003 <deepaksingh76152@gmail.com>
2025-03-31 16:59:43 +0530

    Object Practice

--- Practice/ObjectPrac.js
+++ Practice/ObjectPrac.js
@@ -1,10 +1,24 @@
 
-const person = { firstName: "Deepak", lastName: "Singh", age: 22, eyeColor: "black" }
+const person = { firstName: "Deepak", lastName: "Singh", age: 22, eyeColor: "black", fullName : function(){
+    return this.firstName + " " + this.lastName;
+} }
 // console.log(person);
+
 const animal = new Object();
 animal.name = "Dog";
-animal.color ="Brown";
-animal.weight= 15
-animal.height = 2.5+'feet';
-console.log(animal);
+animal.color = "Brown";
+animal.weight = 15
+animal.height = 2.5 + 'feet';
+// console.log(animal);
+
+//Accessing Object Properties
+console.log(animal.color);
+console.log(animal["color"]);
+
+//JavaScript Object Methods
+console.log(person.fullName());
+
+//JavaScript Objects are Mutable
+
+
 
